ef15849d707ae1a943c7123b9fa70cabb32b626c
msa-utchas <sakibul.utchas@bjitgroup.com>
2023-07-03 11:45:24 +0600

    news details separated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_insider/network/network_manager.dart';
+import 'package:news_insider/news_details.dart';
 
 void main() => runApp(const MyApp());
 
@@ -93,64 +94,6 @@ class NewsListItem extends StatelessWidget {
                 },
                 child: const Text('View Details'),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class NewsDetails extends StatelessWidget {
-  final Article article;
-
-  const NewsDetails({Key? key, required this.article}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('News Details'),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (article.urlToImage != null)
-                Image.network(
-                  article.urlToImage!,
-                  height: 200,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-              const SizedBox(height: 8),
-              Text(
-                article.title ?? '',
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                'By ${article.author ?? 'Unknown'} | ${article.source?.name ?? 'Unknown'} \n ${article.publishedAt ?? ''}',
-                style: const TextStyle(
-                  fontSize: 14,
-                  color: Colors.grey,
-                ),
-              ),
-              const SizedBox(height: 8),
-              SingleChildScrollView(
-                child: Text(
-                  article.description ?? '',
-                  style: const TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 8),
             ],
           ),
         ),
